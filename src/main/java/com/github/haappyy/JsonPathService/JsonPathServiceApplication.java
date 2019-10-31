26186fdd5abcaa1faecfb99f26877b3c9ac9b572
haappyy <57199984+haappyy@users.noreply.github.com>
2019-10-31 04:57:10 +0100

    Add the spring boot project structure

---
+++ src/main/java/com/github/haappyy/JsonPathService/JsonPathServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.haappyy.JsonPathService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPathServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonPathServiceApplication.class, args);
+	}
+
+}
