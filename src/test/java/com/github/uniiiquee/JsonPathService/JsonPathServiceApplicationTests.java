2644269622b097d64033403d76808d6a4d3a0826
haappyy <57199984+haappyy@users.noreply.github.com>
2019-10-31 06:48:50 +0100

    Add tests to JsonPathService

--- src/test/java/com/github/uniiiquee/JsonPathService/JsonPathServiceApplicationTests.java
+++ src/test/java/com/github/uniiiquee/JsonPathService/JsonPathServiceApplicationTests.java
@@ -1,13 +1,37 @@
 package com.github.uniiiquee.JsonPathService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JsonPathServiceApplicationTests {
 
+	@Autowired JsonPathService jsonPathService;
+	
 	@Test
-	void contextLoads() {
+	void isDefinite() {
+		assertTrue(jsonPathService.isDefinte("$.store.book"));
+	}
+	
+	@Test
+	void isIndefinite() {
+		assertFalse(jsonPathService.isDefinte("$..store.book"));
+	}
+	
+	@Test
+	void isGetAuthor() {
+		String json = "            {\r\n" + 
+				"                \"category\": \"reference\",\r\n" + 
+				"                \"author\": \"Test\",\r\n" + 
+				"                \"title\": \"fghfhgfh\",\r\n" + 
+				"                \"price\": 7.95\r\n" + 
+				"            }";
+		assertEquals("Test", jsonPathService.evaluteDefiniteJsonPath("$.author", json));
 	}
 
 }
