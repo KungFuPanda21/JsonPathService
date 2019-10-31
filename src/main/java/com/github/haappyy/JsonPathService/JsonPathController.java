85dd799e6b164fafa85fa62b854d63bccbb12645
haappyy <57199984+haappyy@users.noreply.github.com>
2019-10-31 05:46:07 +0100

    Add the request body to the paramenter list

--- src/main/java/com/github/haappyy/JsonPathService/JsonPathController.java
+++ src/main/java/com/github/haappyy/JsonPathService/JsonPathController.java
@@ -2,6 +2,7 @@ package com.github.haappyy.JsonPathService;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/")
 public class JsonPathController {
 	
+	
+	/**
+	 * Evaluates the JSON path expression provided via the jsonPathExpression parameter.
+	 * @param jsonPathExpression the expression which is applied to the provided json.
+	 * @param json the input json object.
+	 * @return the result of the json path evaluation.
+	 */
 	@ResponseBody
 	@RequestMapping(value = "/jsonPath", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)
-	public ResponseEntity<String> evaluteJsonPath(@RequestParam(name = "jsonPathExpression") String jsonPathExpression) {
+	public ResponseEntity<String> evaluteJsonPath(@RequestParam(name = "jsonPathExpression") String jsonPathExpression, @RequestBody String json) {
 		return ResponseEntity.status(501).build();
 	}
 }
